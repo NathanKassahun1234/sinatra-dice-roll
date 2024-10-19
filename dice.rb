34f25a6e4655489a732d2cd852f6f70d006fef26
NathanKassahun1234 <Kassahunny@uchicago.edu>
2024-10-19 22:45:26 +0000

    break

--- dice.rb
+++ dice.rb
@@ -6,3 +6,26 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+get("/") do 
+  "
+  <h1>Dice Roll</h1>
+
+  <ul>
+  <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    </ul>
+  "
+end 
+get("/") do
+  erb(:elephant)
+end
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+erb(:two_six)
+end
--- lucky_number.rb
+++ lucky_number.rb
@@ -9,22 +9,4 @@ end
 get("/giraffe") do 
   "hopefully this shows up"
 end 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
-end
-get("/dice/2/10") do
-first_die =rand(1..10)
-second_die =rand(1..10)
-sum = first_die + second_die
 
-outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-"<h1>2d10/<h1>
-  <p>#{outcome}</p>"
-end
